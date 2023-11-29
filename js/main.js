4a8dc194eca7c23cd46f04d04c8400b755671544
matiaspuentes12 <matiaspuentes12@gmail.com>
2023-11-29 12:44:46 -0300

    Se realizaron las correciones marcadas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 let vidaScorpion = 100;
 let vidaSubZero = 100;
 let round = 0;
+let golpeSubZero = Math.ceil(Math.random() * 20);
+let golpeScorpion = Math.ceil(Math.random() * 20);
+function pelea(){
+    if (golpeScorpion === golpeSubZero){
+        console.log("Esquivado.");
+    }
+    else if (golpeSubZero > golpeScorpion){
+        vidaScorpion -= golpeSubZero;
+        console.log(`SubZero golpea con una fuerza de ${golpeSubZero}.`);
+        console.log(`La vida de Scorpion baja a ${vidaScorpion} puntos.`);
+    }
+    else{
+        vidaSubZero -= golpeScorpion;
+        console.log(`Scorpion golpea con una fuerza de ${golpeScorpion}.`);
+        console.log(`La vida de SubZero baja a ${vidaSubZero} puntos.`);
+    }
+}
 
 while((vidaScorpion > 0) && (vidaSubZero > 0)) {
-    let golpeSubZero = Math.ceil(Math.random() * 20);
-    let golpeScorpion = Math.ceil(Math.random() * 20);
     round += 1;
 
     if (round === 1){
@@ -15,27 +30,12 @@ while((vidaScorpion > 0) && (vidaSubZero > 0)) {
         console.log(" ");
         console.log(`Round ${round}`);
     }
-
-    function pelea(){
-        if (golpeScorpion === golpeSubZero){
-            console.log("Esquivado.");
-        }
-        else if (golpeSubZero > golpeScorpion){
-            vidaScorpion -= golpeSubZero;
-            console.log(`SubZero golpea con una fuerza de ${golpeSubZero}.`);
-            console.log(`La vida de Scorpion baja a ${vidaScorpion} puntos.`);
-        }
-        else{
-            vidaSubZero -= golpeScorpion;
-            console.log(`Scorpion golpea con una fuerza de ${golpeScorpion}.`);
-            console.log(`La vida de SubZero baja a ${vidaSubZero} puntos.`);
-        }
-    }
     pelea();  
     }
     console.log("--------------------------------------");
     console.log("El ganador es:");
-    if (vidaScorpion >= 0){
+
+    if (vidaScorpion <= 0){
         console.log("SubZero!");
         }
         else{
