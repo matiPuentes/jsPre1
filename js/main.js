e0f26dfb7f9fe8f9d7e928f4bd25a2ed3371d77c
matiaspuentes12 <matiaspuentes12@gmail.com>
2023-12-14 22:34:41 -0300

    Segunda entrega

--- js/main.js
+++ js/main.js
@@ -1,43 +1,34 @@
-let vidaScorpion = 100;
-let vidaSubZero = 100;
-let round = 0;
-let golpeSubZero = Math.ceil(Math.random() * 20);
-let golpeScorpion = Math.ceil(Math.random() * 20);
-function pelea(){
-    if (golpeScorpion === golpeSubZero){
-        console.log("Esquivado.");
-    }
-    else if (golpeSubZero > golpeScorpion){
-        vidaScorpion -= golpeSubZero;
-        console.log(`SubZero golpea con una fuerza de ${golpeSubZero}.`);
-        console.log(`La vida de Scorpion baja a ${vidaScorpion} puntos.`);
-    }
-    else{
-        vidaSubZero -= golpeScorpion;
-        console.log(`Scorpion golpea con una fuerza de ${golpeScorpion}.`);
-        console.log(`La vida de SubZero baja a ${vidaSubZero} puntos.`);
-    }
-}
 
-while((vidaScorpion > 0) && (vidaSubZero > 0)) {
-    round += 1;
 
-    if (round === 1){
-        console.log("Round 1");
-        console.log("Fight!");
-        console.log("-----------------------------------");
-    }else{
-        console.log(" ");
-        console.log(`Round ${round}`);
-    }
-    pelea();  
-    }
-    console.log("--------------------------------------");
-    console.log("El ganador es:");
+const peleadores = [
+    {nombre: "SUB ZERO",
+     equipo: "Lin Kuei",
+     poder: "Hielo",
+     ataque: function(){
+      console.log(`${this.nombre } ataca con poder de ${this.poder}`)}},
 
-    if (vidaScorpion <= 0){
-        console.log("SubZero!");
-        }
-        else{
-            console.log("Scorpion!");
-        }
+     {nombre: "SCORPION",
+     equipo: "Shirai Ryu",
+     poder: "Fuego",
+     ataque: function(){
+      console.log(`${this.nombre } ataca con poder de ${this.poder}`)}},
+    
+     {nombre: "RAIDEN",
+     equipo: "Tierra",
+     poder: "Rayo",
+     ataque: function(){
+      console.log(`${this.nombre } ataca con poder de ${this.poder}`)}},
+
+     {nombre: "LIU KANG",
+     equipo: "Tierra",
+     poder: "Dragón",
+     ataque: function(){
+      console.log(`${this.nombre } ataca con poder de ${this.poder}`)}},
+     
+]
+
+
+peleadores.forEach(function (peleador){
+    console.table(peleador);})
+
+ let peleador = prompt("Elije a tu peleador: \n -Sub Zero \n -Scorpion \n -Raiden \n -Liu Kang")
